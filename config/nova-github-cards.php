86b5412c358ab9b1cb01f8451ed117be57afd415
Claudio-Emmolo <claudioemmolo90@gmail.com>
2023-08-04 10:37:06 +0200

    Add config file

---
+++ config/nova-github-cards.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'vendor' => 'The-3Labs-Team',
+    'repository' => 'nospoiler',
+    'branch' => 'master',
+    'per_page' => 5,
+    'cache' => 3600, //in seconds
+
+    'icons' => [
+        'error' => [
+            'icon' => 'x-circle',
+            'iconClass' => 'text-red-500',
+        ],
+        'success' => [
+            'icon' => 'check-circle',
+            'iconClass' => 'text-green-500',
+        ],
+    ],
+];
